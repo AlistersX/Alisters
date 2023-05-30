a797644230cb154d401878017d1d9a9730cb98ba
Kevin <kevin.liu@clover.com>
2023-05-30 14:45:53 -0400

    styles working

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import MovieCard from '../components/MovieCard';
 import FavoriteMovies from './FavoriteMovies';
+import "../App.css"
 
 const API_URL = 'http://www.omdbapi.com?apikey=b91b1458';
 
