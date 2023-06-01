4e07e965390ad14721e401d52c55331b849ed377
kevin-liu <liu.kevin1997@gmail.com>
2023-06-01 13:10:52 -0400

    started adding favorites

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 function MovieCard({ movie }) {
 
+
   return (<>
     <div className='movie'>
       <div>
--- src/routes/MovieDetails.js
+++ src/routes/MovieDetails.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 function MovieDetails() {
+    const [favorites, setFavorites] = useState([])
 
-  const [movie, setMovie] = useState(null);
-  const params = useParams();
-
-  useEffect(() => {
+    const [movie, setMovie] = useState(null);
+    const params = useParams();
+    
+    useEffect(() => {
     const fetchMovie = async () => {
       try {
         const res = await fetch(`http://www.omdbapi.com?apikey=b91b1458&t=${params.Title}`);
@@ -17,17 +18,26 @@ function MovieDetails() {
     };
     fetchMovie();
   }, [params.Title]);
+  
+  const saveFavorite = () => {
+    setFavorites(movie)
+  }
+  console.log(favorites)
+
 
   if (movie === null) {
     return <div>Loading...</div>;
   }
-
+  
   return (
     <>
       <div>
         <h1>{movie.Title}</h1>
         <h3>{movie.Year}</h3>
          <img src={movie.Poster !== 'N/A' ? movie.Poster : 'http://via.placeholder.com/400'} alt={movie.Title}/>
+      </div>
+      <div>
+        <button onClick={saveFavorite}>Favorite</button>
       </div>
     </>
   );
